3e60f4bf6a95455c21bea981289694d72cc84428
MajoJojo <garces-1992@hotmail.com>
2024-02-28 14:33:37 -0500

    titles apparition

--- javaScript.js
+++ javaScript.js
@@ -6,8 +6,6 @@ const $$ =  (selector) => document.querySelectorAll(selector);
 // http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
 const clearTable = (selector) => $(selector).innerHTML = ''
 
-
-
 const urlBase = `https://gateway.marvel.com/v1/public/`
 const ts = 1
 const publicKey = "3dcf1982b6eb3862570fd32b036be586"
@@ -18,7 +16,6 @@ let orderBy= "title"
 let offSet= 0
 let search=""
 
-
 let page = 1
 let urlAPI = `https://gateway.marvel.com/v1/public/${type}?limit=20&ts=${ts}&apikey=${publicKey}&hash=${hash}&offset=${offSet}&orderBy=${orderBy}`
 
@@ -26,9 +23,7 @@ let datos = []
 
 async function getMarvelComics() {
     try {
-        
         const searchData = search ? `&${type = "characters" ? `name` : `title`}StartsWith=${search}` : ""
-        
         urlAPI =`https://gateway.marvel.com/v1/public/${type}?limit=20&ts=${ts}&apikey=${publicKey}&hash=${hash}&offset=${offSet}&orderBy=${orderBy}${searchData}`
         console.log("cambios aqui", urlAPI)
         const response = await fetch(urlAPI)
@@ -38,11 +33,9 @@ async function getMarvelComics() {
     } catch (error) {
         console.log(error);
     }
-    
-    // return data.data.results   ("image_not_available"))
-    // renderComics()
-    renderCharacters()
+    renderComics()
 }
+
 function renderComics() {
     clearTable(".mainTable")
     datos.forEach((comic) => {
@@ -55,25 +48,8 @@ function renderComics() {
                 <p class="comicTitle font-semibold">${comic.title}</p>
         </div>     
                 `
-        })
-    }
-
-  
-
-function renderCharacters() {
-    clearTable(".mainTable")
-    datos.forEach((character) => {
-        
-        $(".mainTable").innerHTML += `
-        <div class = "itemBox">
-                <div class=" w-48 items-center">
-                    <img  src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
-                    <p class="characterTitle font-semibold">${character.name}</p> 
-                </div> 
-        </div> 
-        `
-        })
-    }
+    })
+}
 
 $("#orderComic").onchange = function (e) {
     if (type=="characters" && e.target.value=="title"){
@@ -99,8 +75,6 @@ $("#typeComic").onchange = function (e) {
         orderBy="title"
     }
     console.log(`order by`,orderBy)
-
-
 }
 $("#searchImput").onchange = function (e) {
     search = e.target.value
@@ -109,36 +83,24 @@ $("#search-button").onclick = function (e) {
     getMarvelComics()
 }
 
-
-//imprimir personaje
-
-
-// const printCharacter = async() => {
-//     const characters = await getMarvelComics()
-//     clearTable(".mainTable")
-//     for(let character of characters){
-//         
-//     }  
-// }
-// imprimir comic
-// const printComic = async() => {
-//     const comics = await getMarvelComics()
-//     clearTable(".mainTable")
-//     for(let comic of comics){
-
-// }
-
-
-
-
-
-
 const pageFirst = document.querySelector("page-first");
-const pageNext = document.querySelector("page-next");
-const pagePrev = document.querySelector("page-prev");
+
 const pageLast = document.querySelector("page-last");
 
-
-window.addEventListener("load", () => {
+$(".page-prev").onclick = function (e) {
+    offSet -= 20
     getMarvelComics()
-})
+}
+$(".page-next").onclick = function (e) {
+    offSet += 20
+    getMarvelComics()
+}
+
+
+
+const initializeApp = () => {
+    getMarvelComics()
+    renderComics()
+}
+
+window.addEventListener("load", initializeApp)
