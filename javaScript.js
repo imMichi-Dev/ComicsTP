13579678286a9b2018c47a233897562bf4dcc877
MajoJojo <garces-1992@hotmail.com>
2024-03-01 03:20:09 -0500

    identation and order.

--- javaScript.js
+++ javaScript.js
@@ -1,26 +1,25 @@
-const $ =  (selector) => document.querySelector(selector);
-const $$ =  (selector) => document.querySelectorAll(selector);
+//BASIC FUNCTIONS AND VARIABLES
+    const $ =  (selector) => document.querySelector(selector);
+    const $$ =  (selector) => document.querySelectorAll(selector);
 
-// const url = "https://api.example.com/v1/data";
+    const clearTable = (selector) => $(selector).innerHTML = ''
 
-// http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
-const clearTable = (selector) => $(selector).innerHTML = ''
+    const urlBase = `https://gateway.marvel.com/v1/public/`
+    const ts = 1
+    const publicKey = "3dcf1982b6eb3862570fd32b036be586"
+    const hash= "566d51213e3f642444b9dfdff2db691d"
 
-const urlBase = `https://gateway.marvel.com/v1/public/`
-const ts = 1
-const publicKey = "3dcf1982b6eb3862570fd32b036be586"
-const hash= "566d51213e3f642444b9dfdff2db691d"
+    let type= "comics"
+    let orderBy= "title"
+    let offSet= 0
+    let search=""
 
-let type= "comics"
-let orderBy= "title"
-let offSet= 0
-let search=""
+    let page = 1
+    let urlAPI = `https://gateway.marvel.com/v1/public/${type}?limit=20&ts=${ts}&apikey=${publicKey}&hash=${hash}&offset=${offSet}&orderBy=${orderBy}`
 
-let page = 1
-let urlAPI = `https://gateway.marvel.com/v1/public/${type}?limit=20&ts=${ts}&apikey=${publicKey}&hash=${hash}&offset=${offSet}&orderBy=${orderBy}`
+    let datos = []
 
-let datos = []
-
+//FETCHING
 async function getMarvelComics() {
     try {
         const searchData = search ? `&${type = "characters" ? `name` : `title`}StartsWith=${search}` : ""
@@ -34,223 +33,217 @@ async function getMarvelComics() {
         console.log(error);
     }
     renderComics()
-
 }
 
-function renderComics() {
-    clearTable(".mainTable")
-    $(".resultCount").textContent = `${datos.length} Resultados`
-    datos.forEach((comic) => {
-        if(type=="characters"){
-            console.log("holi")
-            $(".mainTable").innerHTML += `
-                    <div class="itemBox min-w-40 max-w-48 m-5" onclick="getCharacterId(${comic.id})">
-                            <div class="w-48 items-center m-8">
-                                <img  src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.name}">
-                            </div>
-                            <p class="comicTitle font-semibold">${comic.name}</p>
-                    </div> 
-                    `
+//FILTERS
+    $("#orderComic").onchange = function (e) {
+        if (type=="characters" && e.target.value=="title"){
+            orderBy= "name"
+            return
+        }else if(type=="characters"&&  e.target.value=="-title") {
+            orderBy= "-name"
+            return
         }else{
-            console.log("holi")
-            $(".mainTable").innerHTML += `
-                    <div class = "itemBox  min-w-40 max-w-48 m-5" onclick="getComicId(${comic.id})">
-                            <div class="w-48 items-center">
-                                <img  src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}">
-                            </div>
-                            <p class="comicTitle font-semibold">${comic.title}</p>
-                    </div>     
-                    `
+            orderBy= e.target.value
         }
-    })
-}
-
-$("#orderComic").onchange = function (e) {
-    if (type=="characters" && e.target.value=="title"){
-        orderBy= "name"
-        return
-    }else if(type=="characters"&&  e.target.value=="-title") {
-        orderBy= "-name"
-        return
-    }else{
-        orderBy= e.target.value
+        console.log(`order by`,orderBy)
     }
-    console.log(`order by`,orderBy)
-}
-$("#typeComic").onchange = function (e) {
-    type= e.target.value
-    if (type=="characters" && orderBy=="title"){
-        orderBy= "name"
-        return
-    }else if(type=="characters" && orderBy=="-title") {
-        orderBy= "-name"
-        return
-    }else{
-        orderBy="title"
+    $("#typeComic").onchange = function (e) {
+        type= e.target.value
+        if (type=="characters" && orderBy=="title"){
+            orderBy= "name"
+            return
+        }else if(type=="characters" && orderBy=="-title") {
+            orderBy= "-name"
+            return
+        }else{
+            orderBy="title"
+        }
+        console.log(`order by`,orderBy)
     }
-    console.log(`order by`,orderBy)
-}
-$("#searchImput").onchange = function (e) {
-    search = e.target.value
-}
-$("#search-button").onclick = function (e) {
-    getMarvelComics()
-}
-
-const pageFirst = document.querySelector("page-first");
-
-const pageLast = document.querySelector("page-last");
-
-$(".page-prev").onclick = function (e) {
-    offSet -= 20
-    getMarvelComics()
-}
-$(".page-next").onclick = function (e) {
-    offSet += 20
-    getMarvelComics()
-}
-
-async function getCharacterId(id){
-    try {
-        const response = await fetch(`https://gateway.marvel.com/v1/public/characters/${id}?&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-        console.log(response)
-        const data = await response.json()
-        datos = data.data.results  
-    } catch (error) {
-        console.log(error);
+    $("#searchImput").onchange = function (e) {
+        search = e.target.value
     }
-    printCharacterDescription(datos)
-    getCharacterComics(id)
-}
-async function getCharacterComics(id) {
-    try {
-        const response = await fetch(`https://gateway.marvel.com/v1/public/characters/${id}/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-        const data = await response.json()
-        const comics = data.data.results
-        printCharacterComics(comics)
-        console.log(comics)
-    } catch (error) {
-        console.error(error)
+    $("#search-button").onclick = function (e) {
+        getMarvelComics()
     }
-}
+//PAGES BUTTONS
+    const pageFirst = document.querySelector("page-first");
 
+    const pageLast = document.querySelector("page-last");
 
-function printCharacterComics (comics){
-    $(".resultComicsCount").textContent = `${comics.length} Resultados`
-    for (const comic of comics) {
-        console.log("halo");
+    $(".page-prev").onclick = function (e) {
+        offSet -= 20
+        getMarvelComics()
+    }
+    $(".page-next").onclick = function (e) {
+        offSet += 20
+        getMarvelComics()
+    }
+//GETTING ID'S
+    async function getComicId(id){
+        try {
+            const response = await fetch(`https://gateway.marvel.com/v1/public/comics/${id}?&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+            const data = await response.json()
+            datos = data.data.results  
+        } catch (error) {
+            console.log(error);
+        }
+        printComicDescription(datos);
+        getComicCharacters(id)
+    }
+    async function getComicCharacters(id) {
+        try {
+            const response = await fetch(`https://gateway.marvel.com/v1/public/comics/${id}/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+            const data = await response.json()
+            const characters = data.data.results
+            printComicCharacters(characters)
+            console.log(characters)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    async function getCharacterId(id){
+        try {
+            const response = await fetch(`https://gateway.marvel.com/v1/public/characters/${id}?&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+            console.log(response)
+            const data = await response.json()
+            datos = data.data.results  
+        } catch (error) {
+            console.log(error);
+        }
+        printCharacterDescription(datos)
+        getCharacterComics(id)
+    }
+    async function getCharacterComics(id) {
+        try {
+            const response = await fetch(`https://gateway.marvel.com/v1/public/characters/${id}/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+            const data = await response.json()
+            const comics = data.data.results
+            printCharacterComics(comics)
+            console.log(comics)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+//RENDERS
+    function renderComics() {
+        clearTable(".mainTable")
+        $(".resultCount").textContent = `${datos.length} Resultados`
+        datos.forEach((comic) => {
+            if(type=="characters"){
+                console.log("holi")
+                $(".mainTable").innerHTML += `
+                        <div class="itemBox min-w-40 max-w-48 m-5" onclick="getCharacterId(${comic.id})">
+                                <div class="w-48 items-center m-8">
+                                    <img  src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.name}">
+                                </div>
+                                <p class="comicTitle font-semibold">${comic.name}</p>
+                        </div> 
+                        `
+            }else{
+                console.log("holi")
+                $(".mainTable").innerHTML += `
+                        <div class = "itemBox  min-w-40 max-w-48 m-5" onclick="getComicId(${comic.id})">
+                                <div class="w-48 items-center">
+                                    <img  src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}">
+                                </div>
+                                <p class="comicTitle font-semibold">${comic.title}</p>
+                        </div>     
+                        `
+            }
+        })
+    }
+
+    function printComicDescription (datos)  {
+        clearTable(".mainTable")
+        for(const dato of datos){
+        const comicDate = dato.dates.find(date => date.type === "onsaleDate").date;
+        const formattedDate = new Date(comicDate).toDateString();
+    
+        const writers = dato.creators.items.filter(creator => creator.role === "writer");
+        const writerNames = writers.map(writer => writer.name).join(', ');
+    
         $(".mainTable").innerHTML += `
-        <div class = "itemBox  min-w-40 max-w-48 m-5" onclick="getComicId(${comic.id})">
-                <div class="w-48 items-center">
-                    <img  src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}">
-                </div>
-                <p class="comicTitle font-semibold">${comic.title}</p>
-        </div>     
-        `
-    }}
-
-function printCharacterDescription (datos)  {
-    clearTable(".mainTable")
-    for(const dato of datos){
-    if (dato.description && dato.description.length > 0) {
-        $(".mainTable").innerHTML += `
-        <div class="flex flex-col w-full">
+        <div class="flex flex-col">
             <div class="flex flex-row justify-start w-full">
                 <div class="min-w-48 max-w-80">
-                    <img src="${dato.thumbnail.path}.${dato.thumbnail.extension}" alt="${dato.name}">
+                    <img src="${dato.thumbnail.path}.${dato.thumbnail.extension}" alt="${dato.title}">
                 </div>
                 <div class="text-left ml-6">
-                    <p class="font-bold">${dato.name}</p>
+                    <p class="font-bold">${dato.title}</p>
+                    <p class="font-bold">Publicado:</p>
+                    <p>${formattedDate}</p>
+                    <p class="font-bold">Guionistas: </p>
+                    <spam>${writerNames}</spam>
+                    <p class="font-bold">Descripción: </p>
                     <spam>${dato.description}<spam>
                 </div>
             </div>
             <div class="text-left">
-                <p class="font-bold">Comics</p>
-                <p class="resultComicsCount"></p>
+                <p class="font-bold">Personajes</p>
+                <p class="resultCharactersCount"></p>
             </div>
         </div>
         `
     }}
-}
 
-async function getComicId(id){
-    try {
-        const response = await fetch(`https://gateway.marvel.com/v1/public/comics/${id}?&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-        const data = await response.json()
-        datos = data.data.results  
-    } catch (error) {
-        console.log(error);
-    }
-    printComicDescription(datos);
-    getComicCharacters(id)
-}
-async function getComicCharacters(id) {
-    try {
-        const response = await fetch(`https://gateway.marvel.com/v1/public/comics/${id}/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-        const data = await response.json()
-        const characters = data.data.results
-        printComicCharacters(characters)
-        console.log(characters)
-    } catch (error) {
-        console.error(error)
-    }
-}
+    function printComicCharacters (characters){
+        $(".resultCharactersCount").textContent = `${characters.length} Resultados`
+        for (const character of characters) {
+            console.log("halo");
+    
+    
+                $(".mainTable").innerHTML += `
+                <div>
+                    <div class="w-48 justify-items-start items-center m-8">
+                        <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
+                    </div>
+                    <p class="comicTitle font-semibold">${character.name}</p>
+                </div>`;
+     
+    
+        }}
 
-
-function printComicDescription (datos)  {
-    clearTable(".mainTable")
-    for(const dato of datos){
-    const comicDate = dato.dates.find(date => date.type === "onsaleDate").date;
-    const formattedDate = new Date(comicDate).toDateString();
-
-    const writers = dato.creators.items.filter(creator => creator.role === "writer");
-    const writerNames = writers.map(writer => writer.name).join(', ');
-
-    $(".mainTable").innerHTML += `
-    <div class="flex flex-col">
-        <div class="flex flex-row justify-start w-full">
-            <div class="min-w-48 max-w-80">
-                <img src="${dato.thumbnail.path}.${dato.thumbnail.extension}" alt="${dato.title}">
-            </div>
-            <div class="text-left ml-6">
-                <p class="font-bold">${dato.title}</p>
-                <p class="font-bold">Publicado:</p>
-                <p>${formattedDate}</p>
-                <p class="font-bold">Guionistas: </p>
-                <spam>${writerNames}</spam>
-                <p class="font-bold">Descripción: </p>
-                <spam>${dato.description}<spam>
-            </div>
-        </div>
-        <div class="text-left">
-            <p class="font-bold">Personajes</p>
-            <p class="resultCharactersCount"></p>
-        </div>
-    </div>
-    `
-}
-}
-function printComicCharacters (characters){
-    $(".resultCharactersCount").textContent = `${characters.length} Resultados`
-    for (const character of characters) {
-        console.log("halo");
-
-
-            $(".mainTable").innerHTML += `
-            <div>
-                <div class="w-48 justify-items-start items-center m-8">
-                    <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
+        function printCharacterComics (comics){
+            $(".resultComicsCount").textContent = `${comics.length} Resultados`
+            for (const comic of comics) {
+                console.log("halo");
+                $(".mainTable").innerHTML += `
+                <div class = "itemBox  min-w-40 max-w-48 m-5" onclick="getComicId(${comic.id})">
+                        <div class="w-48 items-center">
+                            <img  src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}">
+                        </div>
+                        <p class="comicTitle font-semibold">${comic.title}</p>
+                </div>     
+                `
+        }}
+        
+        function printCharacterDescription (datos)  {
+            clearTable(".mainTable")
+            for(const dato of datos){
+            if (dato.description && dato.description.length > 0) {
+                $(".mainTable").innerHTML += `
+                <div class="flex flex-col w-full">
+                    <div class="flex flex-row justify-start w-full">
+                        <div class="min-w-48 max-w-80">
+                            <img src="${dato.thumbnail.path}.${dato.thumbnail.extension}" alt="${dato.name}">
+                        </div>
+                        <div class="text-left ml-6">
+                            <p class="font-bold">${dato.name}</p>
+                            <spam>${dato.description}<spam>
+                        </div>
+                    </div>
+                    <div class="text-left">
+                        <p class="font-bold">Comics</p>
+                        <p class="resultComicsCount"></p>
+                    </div>
                 </div>
-                <p class="comicTitle font-semibold">${character.name}</p>
-            </div>`;
- 
-
-    }}
+                `
+            }}
+        }
 
 const initializeApp = () => {
     getMarvelComics()
-    
-
 }
-
 window.addEventListener("load", initializeApp)
